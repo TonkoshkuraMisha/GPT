0271b86fce2e3264c7db3cae19017519dabaf5ff
Tonkoshkura Misha <tonkoshkura.misha@google.com>
2023-06-28 14:01:01 +0300

    added inetviews files

--- temp0.py
+++ temp0.py
@@ -1,22 +1,23 @@
-commands = ['git commit -m "Fixed"',
-            'git commit',
-            'git add /src',
-            'docker-compose up']
+import collections
 
-for command in commands:
-    match command.split():
-        case 'git', 'add', path:
-            print(f'В индекс добавлены файлы по пути: {path}.')
-        case 'git', 'commit':
-            print('Файлы закоммичены.')
-        case 'git', 'commit', '-m', message:
-            print(f'Файлы закоммичены с сообщением {message}')
-        case 'git', 'push', repository, branch:
-            print(f'Файлы запушены в {repository} в ветку {branch}.')
-        case _:
-            print('Неизвестная команда.')
 
-#  Файлы закоммичены с сообщением "Fixed"
-#  Файлы закоммичены.
-#  В индекс добавлены файлы по пути: /src.
-#  Неизвестная команда.
+def find_most_common_word(file_path):
+    word_counts = collections.Counter()
+
+    with open(file_path, "r") as file:
+        content = file.read()
+        content = content.replace(",", "").replace(".", "")  # Удаление разделителей
+        words = content.lower().split()
+
+        word_counts.update(words)
+
+    most_common_word, word_count = word_counts.most_common(1)[0]
+
+    return most_common_word, word_count
+
+
+# Пример использования
+file_path = "file.txt"
+word, word_count = find_most_common_word(file_path)
+print("word =", word)
+print("word_count =", word_count)
